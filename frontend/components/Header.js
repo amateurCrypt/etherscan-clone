2538c9a4d977d719daaba17ccbc74497b794703a
moh <muhammad.qasim1@live.com>
2023-04-19 17:06:33 -0400

    edited index.js (removed out of the box code). import components to header.js file. (till 25m27s)

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -2,3 +2,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Images from "next/image"
 import styles from "@/styles/Home.module.css";
+import Logo from "../public/assets/logo.png"
+
+import Logo from "../public/assets/logo.png";
+
+export default function Header() {
+    const [ethPrice, setEthPrice] = useState("") ;
+}
