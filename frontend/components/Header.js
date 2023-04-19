6b5f1dd45d18bb111b768f9768d6e6392fad9363
moh <muhammad.qasim1@live.com>
2023-04-19 17:14:45 -0400

    introduced useeffect hook and section which will allow the app to render (till 27m00s)

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -7,5 +7,19 @@ import Logo from "../public/assets/logo.png"
 import Logo from "../public/assets/logo.png";
 
 export default function Header() {
-    const [ethPrice, setEthPrice] = useState("") ;
+    const [ethPrice, setEthPrice] = useState("");
+
+    useEffect(() => {
+        const getEthPrice = async () => {
+            const response = await axios.get('http://localhost:5001/getethprice', {});
+            setEthPrice( response. data.usdPrice);
+        }
+        getEthPrice(); 
+    });
+    return (
+        <section className={styles.header}>
+            <section className={styles.topHeader}></section>
+        </section>
+
+    )
 }
