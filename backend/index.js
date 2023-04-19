59573d7c4249a5219f931f88670606e4e42d7fae
moh <muhammad.qasim1@live.com>
2023-04-19 14:50:31 -0400

    added another end point. this end point does 2 requests and they are to get block data.this is final part of backend server (till 20m37s)

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,63 @@ app.get("/getethprice", async (req, res) => {
     }
 })
 
+app.get("/getblockinfo", async (req, res) => {
+    try {
+        const latestBlock = await Moralis.EvmApi.block. getDateToBlock({
+            date: Date.now(),
+            chain: "0x1"
+        })
+    
+        let blockNrOrParentHash = latestBlock.toJSON().block;
+        let previousBlockInfo = [];
+
+        for (let i =o; i < 5; i++) {
+            const previousBlockNrs = await Moralis.EvmApi.block.getBlock({
+                chain: "0x1",
+                blockNumberOrHash: blockNrOrParentHash
+            })
+
+            blockNrOrParentHash = previousBlockNrs.toJSON().parent_hash;
+
+            if(i == 0) {
+                previousBlockInfo.push({
+                    transactions: previousBlockNrs.toJSON().transactions.map((i) => {
+                        return {
+                            transactionHash: i.hash,
+                            time: i.block_timestamp,
+                            fromAddress: i.from_address,
+                            toAddress: i.to_address,
+                            value: i.value
+                        }
+                    })
+                })
+            }
+
+            previousBlockInfo.push({
+                blockNumber: previousBlockNrs.toJSON.number,
+                totalTransactions: previousBlockNrs.toJSON().transaction_count,
+                gasUsed: previousBlockNrs.toJSON().gas_used,
+                miner: previousBlockNrs.toJSON().miner,
+                time: previousBlockNrs.toJSON().timestamp
+           });
+
+        }
+
+        const response = {
+            latestBlock: latestBlock.toJSON().block,
+            previousBlockInfo
+        };
+
+        return res.status(200).json(response)
+
+    }   catch (e) {
+
+        console.log(`something went wrong ${e}`);
+        return res.status(400).json();
+    }
+
+})
+
 app.get("/address", async (req, res) => {
     try {
         const { query } = req;
@@ -47,7 +104,6 @@ app.get("/address", async (req, res) => {
         return res.status(400).json();
     }
 });
-
 
 Moralis.start({
     apikKey: MORALIS_API_KEY,
